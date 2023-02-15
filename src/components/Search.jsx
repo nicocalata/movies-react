c656801cda95ecbaea07f32a69805953089e6ec0
Nicolás Emilio Calatayud <110576087+nicocalata@users.noreply.github.com>
2023-02-15 04:21:25 -0300

    fix 3

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import styles from "./Search.module.css"
-import {FaSearch} from 'react-icons/fa'
-import { useHistory } from "react-router-dom"
+import { FaSearch } from 'react-icons/fa'
+import { useHistory, useQuery} from "react-router-dom"
 
 export function Search() {
   const query = useQuery();
